7a3330b8ae94cc016693fa5c7c9814070c86a835
Dario <dasemenzato@hotmail.com>
2019-09-30 19:34:33 -0300

    add song listo

--- api/routes/album.routes.js
+++ api/routes/album.routes.js
@@ -12,8 +12,8 @@ var md_upload = multipart({uploadDir: './uploads/albums'})
 api.get('/album/:id', md_auth.ensureAuth, AlbumController.getAlbum);
 api.post('/album', md_auth.ensureAuth, AlbumController.saveAlbum);
 api.get('/albums/:artist?', md_auth.ensureAuth, AlbumController.getAlbums);
-api.put('/album/:id', md_auth.ensureAuth, AlbumController.updateAlbum);
-api.delete('/album/:id', md_auth.ensureAuth, AlbumController.deleteAlbum);
+api.post('/edit-album/:id', md_auth.ensureAuth, AlbumController.updateAlbum);
+api.get('/delete-album/:id', md_auth.ensureAuth, AlbumController.deleteAlbum);
 api.post('/upload-image-album/:id', [md_auth.ensureAuth, md_upload], AlbumController.uploadImage);
 api.get('/get-image-album/:imageFile', AlbumController.getImageFile);
 
